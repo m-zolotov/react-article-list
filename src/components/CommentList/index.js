34de2f735e823436a7514b29a58196ab8c6173d3
Michael Zolotov <m.v.zolotov@gmail.com>
2018-06-22 23:12:56 +0300

    Rewrite CommentList to stateless function

--- src/components/CommentList/index.js
+++ src/components/CommentList/index.js
@@ -1,40 +1,37 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import toggleOpen from '../../decorators/toggleOpen';
 import Comment from '../Comment';
 
 import './style.css';
 
-class CommentList extends Component {
-    static propTypes = {
-        comments: PropTypes.array.isRequired
-    };
+CommentList.propTypes = {
+    comments: PropTypes.array.isRequired
+};
 
-    static defaultProps = {
-        comments: []
-    };
+CommentList.defaultProps = {
+    comments: []
+};
 
-    getBody() {
-        const {comments, isOpen} = this.props;
-        if (!isOpen) return null;
-        if (!comments.length) return <p>No comments yet</p>;
+function getBody({comments, isOpen}) {
+    if (!isOpen) return null;
+    if (!comments.length) return <p>No comments yet</p>;
 
-        return (
-            <ul className="CommentList">
-                {comments.map(comment => <li key={comment.id} className="CommentList__item"><Comment comment={comment} /></li>)}
-            </ul>
-        )
-    };
+    return (
+        <ul className="CommentList">
+            {comments.map(comment => <li key={comment.id} className="CommentList__item"><Comment comment={comment} /></li>)}
+        </ul>
+    )
+}
 
-    render() {
-        const text = this.props.isOpen ? 'Close comments' : 'Open comments';
-        return (
-            <Fragment>
-                <button className="button button--light" onClick={this.props.toggleOpen}>{text}</button>
-                {this.getBody()}
-            </Fragment>
-        )
-    }
+function CommentList ({comments = [], isOpen, toggleOpen}) {
+    const text = isOpen ? 'Hide comments' : 'Show comments';
+    return (
+        <Fragment>
+            <button className="button button--light" onClick={toggleOpen}>{text}</button>
+            {getBody({comments, isOpen})}
+        </Fragment>
+    )
 }
 
 export default toggleOpen(CommentList);
