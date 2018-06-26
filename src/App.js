8584321b664a70b0ee1e0107473121f6c9f40fe8
Michael Zolotov <m.v.zolotov@gmail.com>
2018-06-26 23:20:20 +0300

    Rewrite DayPicker, add grid to the filter-form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import moment from 'moment';
 import Select from 'react-select';
-import DayPickerInput from 'react-day-picker/DayPickerInput';
-import { formatDate, parseDate } from 'react-day-picker/moment';
+import DayPicker, { DateUtils } from 'react-day-picker';
 import ArticleList from "./components/ArticleList";
 
 import {articles} from './fixtures/fixtures';
@@ -12,43 +10,31 @@ import 'react-day-picker/lib/style.css';
 import './style/style.css';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.handleFromChange = this.handleFromChange.bind(this);
-        this.handleToChange = this.handleToChange.bind(this);
-        this.state = {
+    static defaultProps = {
+        numberOfMonths: 2,
+    };
+
+    state = {
+        selection: null
+    };
+
+    getInitialState() {
+        return {
             from: undefined,
             to: undefined,
-            selection: null
         };
     }
 
-    changeSelection = selection => this.setState({selection});
+    handleDayClick = (day) => {
+        const range = DateUtils.addDayToRange(day, this.state);
+        this.setState(range);
+    };
 
-    componentWillUnmount() {
-        clearTimeout(this.timeout);
+    handleResetClick() {
+        this.setState(this.getInitialState());
     }
-    focusTo() {
-        // Focus to `to` field. A timeout is required here because the overlays
-        // already set timeouts to work well with input fields
-        this.timeout = setTimeout(() => this.to.getInput().focus(), 0);
-    }
-    showFromMonth() {
-        const { from, to } = this.state;
-        if (!from) {
-            return;
-        }
-        if (moment(to).diff(moment(from), 'months') < 2) {
-            this.to.getDayPicker().showMonth(from);
-        }
-    }
-    handleFromChange(from) {
-        // Change the from date and focus the "to" input field
-        this.setState({ from });
-    }
-    handleToChange(to) {
-        this.setState({ to }, this.showFromMonth);
-    }
+
+    changeSelection = selection => this.setState({selection});
 
     render() {
         const { from, to } = this.state;
@@ -61,44 +47,31 @@ class App extends Component {
         return (
             <div className="App">
                 <form action="" className="filter-form">
-                    <Select options={options} value={this.state.selection} onChange={this.changeSelection} />
+                    <div className="filter-form-select">
+                        <Select options={options} value={this.state.selection} onChange={this.changeSelection} />
+                    </div>
                     <div className="filter-form-day-picker">
-                        <DayPickerInput
-                            value={from}
-                            placeholder="From"
-                            format="LL"
-                            formatDate={formatDate}
-                            parseDate={parseDate}
-                            dayPickerProps={{
-                                selectedDays: [from, { from, to }],
-                                disabledDays: { after: to },
-                                toMonth: to,
-                                modifiers,
-                                numberOfMonths: 1,
-                                onDayClick: () => this.to.getInput().focus(),
-                            }}
-                            onDayChange={this.handleFromChange}
+                        <p className="filter-form-day-picker__title">
+                            {!from && !to && 'Please select the first day.'}
+                            {from && !to && 'Please select the last day.'}
+                            {from &&
+                            to &&
+                            `Selected from ${from.toLocaleDateString()} to
+                            ${to.toLocaleDateString()}`}{' '}
+                            {from &&
+                            to && (
+                                <button className="link" onClick={this.handleResetClick}>
+                                    Reset
+                                </button>
+                            )}
+                        </p>
+                        <DayPicker
+                            className="Selectable"
+                            numberOfMonths={this.props.numberOfMonths}
+                            selectedDays={[from, { from, to }]}
+                            modifiers={modifiers}
+                            onDayClick={this.handleDayClick}
                         />
-                        {' '}-{' '}
-                        <span className="InputFromTo-to">
-                            <DayPickerInput
-                                ref={el => (this.to = el)}
-                                value={to}
-                                placeholder="To"
-                                format="LL"
-                                formatDate={formatDate}
-                                parseDate={parseDate}
-                                dayPickerProps={{
-                                    selectedDays: [from, { from, to }],
-                                    disabledDays: { before: from },
-                                    modifiers,
-                                    month: from,
-                                    fromMonth: from,
-                                    numberOfMonths: 1,
-                                }}
-                                onDayChange={this.handleToChange}
-                            />
-                        </span>
                     </div>
                 </form>
                 <ArticleList articles={articles} />
