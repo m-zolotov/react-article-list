becd5e63456e1e067612f480804a9e4ee21ddf49
Michael Zolotov <m.v.zolotov@gmail.com>
2018-09-10 19:01:05 +0300

    Add NavLink CommentsPage

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Articles from './components/Routes/Articles';
 import NewArticle from './components/Routes/NewArticle';
 import CommentsPage from './components/Routes/CommentsPage';
 import NotFound from './components/Routes/NotFound';
-import {BrowserRouter as Router, Switch, Route, Redirect, NavLink} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, NavLink} from 'react-router-dom';
 import Counter from './components/Counter';
 
 import './style/style.css';
@@ -18,6 +18,7 @@ class App extends Component {
                         <NavLink to="/counter" className="App__nav-link" activeClassName="App__nav-link--active">Counter</NavLink>
                         <NavLink to="/filter" className="App__nav-link" activeClassName="App__nav-link--active">Filter</NavLink>
                         <NavLink to="/articles" className="App__nav-link" activeClassName="App__nav-link--active">ArticleList</NavLink>
+                        <NavLink to="/comments" className="App__nav-link" activeClassName="App__nav-link--active">CommentsPage</NavLink>
                     </nav>
                     <Switch>
                         <Route path="/counter" component={Counter} />
@@ -25,7 +26,6 @@ class App extends Component {
                         <Route path="/articles/new" component={NewArticle} />
                         <Route path="/articles" component={Articles} />
                         <Route path="/comments" component={CommentsPage} />
-                        {/*<Redirect from="/comments/" to="/comments/1"></Redirect>*/}
                         <Route path="*" component={NotFound} />
                     </Switch>
                 </div>
